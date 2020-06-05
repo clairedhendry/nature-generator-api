8b382985eb165bd0bf30c25f46190e33d799a645
Claire Hendry <claire.d.hendry@gmail.com>
2020-06-05 09:09:46 -0400

    finalizing

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL,
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/nature-generator-test",
-    CLIENT_ORIGIN: 'https://nature-generator-client.now.sh/'
+    CLIENT_ORIGIN: 'https://nature-generator-client.now.sh$/'
 }
